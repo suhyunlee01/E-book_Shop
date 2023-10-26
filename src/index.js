d8b6427e51ad8942048d40a252e041d474a3715b
lsh <leesuhyun05505@gmail.com>
2023-10-26 12:16:46 +0900

    배포

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store = {store}>
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <App />
       </Router>
     </Provider>
