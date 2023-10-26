a4dd054482de53004d4922cba0e8ad4dbca95ee3
lsh <leesuhyun05505@gmail.com>
2023-10-26 11:57:36 +0900

    basename 기본 URL 설정

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store = {store}>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <App />
       </Router>
     </Provider>
