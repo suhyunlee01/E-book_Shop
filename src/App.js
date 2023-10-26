f77065f256d1d4e490716a80baadab4eb34c1058
lsh <leesuhyun05505@gmail.com>
2023-10-26 10:22:21 +0900

    Edit: 라우터 수정

--- src/App.js
+++ src/App.js
@@ -69,44 +69,46 @@ function App() {
         </Container>
       </Navbar>
 
-      {/* Routes가 최상위로, Route들을 관리한다. */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          {/* 디폴트 메인 페이지 */}
-          <Route path='/' element={<Home updateNobels={updateNobels}></Home>}></Route>
+      <Router>
+        {/* Routes가 최상위로, Route들을 관리한다. */}
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            {/* 디폴트 메인 페이지 */}
+            <Route path='/' element={<Home updateNobels={updateNobels}></Home>}></Route>
 
-          {/* /toon 페이지를 하나 만들고, 해당 컴포넌트의 엘리먼트를 설정함 */}
-          {/* Toon 컴포넌트로부터 업데이트한 데이터를 인자로 받아오기 위한 함수 updateToons 전달 */}
-          <Route path='/toon' element={<Toon updateToons={updateToons}></Toon>}></Route>
+            {/* /toon 페이지를 하나 만들고, 해당 컴포넌트의 엘리먼트를 설정함 */}
+            {/* Toon 컴포넌트로부터 업데이트한 데이터를 인자로 받아오기 위한 함수 updateToons 전달 */}
+            <Route path='/toon' element={<Toon updateToons={updateToons}></Toon>}></Route>
 
-          {/* /nobel 페이지를 하나 만들고, 해당 컴포넌트의 엘리먼트를 설정함 */}        
-          <Route path='/about' element={<About></About>}></Route>
+            {/* /nobel 페이지를 하나 만들고, 해당 컴포넌트의 엘리먼트를 설정함 */}        
+            <Route path='/about' element={<About></About>}></Route>
 
-          <Route path="/cart" element={<Cart></Cart>}></Route>
+            <Route path="/cart" element={<Cart></Cart>}></Route>
 
 
-          {/* 웹소설 데이터 id별로 /toon_details 페이지 + /book.id 로 Router 각각 하나씩 만들고 호출하기.. NobelDetails 컴포넌트는 각각의 book 데이터를 props로 받아서 출력 */}
-          {books.map((book)=>{
-            return <Route key={book.id} path={`/nobel_details/${book.id}`} element={<NobelDetails book={book}></NobelDetails>}></Route>
-          })} 
-        
+            {/* 웹소설 데이터 id별로 /toon_details 페이지 + /book.id 로 Router 각각 하나씩 만들고 호출하기.. NobelDetails 컴포넌트는 각각의 book 데이터를 props로 받아서 출력 */}
+            {books.map((book)=>{
+              return <Route key={book.id} path={`/nobel_details/${book.id}`} element={<NobelDetails book={book}></NobelDetails>}></Route>
+            })} 
           
-          {/* 웹툰 데이터 id별로 /toon_details 페이지 + /toon.id 로 Router 각각 하나씩 만들고 호출하기...ToonDetails 컴포넌트는 각각의 toon 데이터를 props로 받아서 출력 */}
-          {toons.map((toon) => {
-            return <Route key={toon.id} path={`/toon_details/${toon.id}`} element={
-              <Context1.Provider value={""}>
-                {/* Context.Provider의 value를 toonDetails 컴포넌트와 그 하위의 컴포넌트 모두가 자신의 데이터처럼 사용할 수 있다. */}
-                <ToonDetails toon={toon}></ToonDetails>
-              </Context1.Provider>
-            }></Route>
-          })}
-          
-          
-          {/* 404에러 path가 정해진 것 외로 넘어가면 해당 페이지로 이동함 */}
-          <Route path='*' element={<div><h1>404에러</h1><br></br>예상치 못한 접근이 발생했어요!🥲</div>}></Route>
+            
+            {/* 웹툰 데이터 id별로 /toon_details 페이지 + /toon.id 로 Router 각각 하나씩 만들고 호출하기...ToonDetails 컴포넌트는 각각의 toon 데이터를 props로 받아서 출력 */}
+            {toons.map((toon) => {
+              return <Route key={toon.id} path={`/toon_details/${toon.id}`} element={
+                <Context1.Provider value={""}>
+                  {/* Context.Provider의 value를 toonDetails 컴포넌트와 그 하위의 컴포넌트 모두가 자신의 데이터처럼 사용할 수 있다. */}
+                  <ToonDetails toon={toon}></ToonDetails>
+                </Context1.Provider>
+              }></Route>
+            })}
+            
+            
+            {/* 404에러 path가 정해진 것 외로 넘어가면 해당 페이지로 이동함 */}
+            <Route path='*' element={<div><h1>404에러</h1><br></br>예상치 못한 접근이 발생했어요!🥲</div>}></Route>
 
-        </Routes>
-      </Suspense>
+          </Routes>
+        </Suspense>
+      </Router>
     </div>
   );
 }
